46e18bf23e3a931e9d0f3442f1f2bc639bc19a40
Varun J <root@5ht2.me>
2022-07-11 18:23:34 +0530

    initial release

---
+++ CHANGELOG.md
@@ -0,0 +1,13 @@
+0.1.0
+
+# Changelog
+
+Initial Release
+
+## Added
+
+## Changed
+
+## Fixed
+
+### Notes
--- dice_parser/__init__.py
+++ dice_parser/__init__.py
@@ -37,4 +37,4 @@ _roller = Roller()
 roll = _roller.roll
 parse = _roller.parse
 
-__verion__ = '0.1.0a'
+__verion__ = '0.1.0'
